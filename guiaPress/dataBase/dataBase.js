bef760a9a465001d1545f033dda6a24dc50443b2
antoniomrrds <arqueiroverd@gmail.com>
2021-11-12 01:02:33 -0300

    mais alguns arquivos 12-11-2021

--- guiaPress/dataBase/dataBase.js
+++ guiaPress/dataBase/dataBase.js
@@ -2,7 +2,8 @@ const  Sequelize =require('sequelize');
 
 const connection = new Sequelize('guiapress','scorpion','Reis23101996',{
     host:'10.0.0.138',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
--- guiaPress/index.js
+++ guiaPress/index.js
@@ -1,36 +1,38 @@
 const express = require('express');
-const app =  express();
-const connection =  require('./dataBase/dataBase')
+const app = express();
+const connection = require('./dataBase/dataBase')
 
 
 const categoriesController = require('./categories/categoriesController')
 const articlesController = require('./articles/articlesController')
 
 
-const Category = require('./categories/Category') 
-const Article = require('./articles/Article') 
+const Category = require('./categories/Category')
+const Article = require('./articles/Article')
 connection
       .authenticate()
       .then(_ => console.log('Conexão bem sucedida com o banco de dados !!! '))
       .catch((error) => console.log(error))
 
 //View engine 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 // Static
 
 app.use(express.static('public'));
 
 //express bodyparser 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/',categoriesController)
-app.use('/',articlesController)
+app.use('/', categoriesController)
+app.use('/', articlesController)
 
-app.get('/',(req,res) => {
-   res.render('index');
+app.get('/', (req, res) => {
+      Article.findAll().then(articles => {
+            res.render('index', { articles: articles });
+      })
 
 })
 
 const port = 1818;
-app.listen(port ,_=> console.log(`O servidor etá on na porta ${port} !!!`))
+app.listen(port, _ => console.log(`O servidor etá on na porta ${port} !!!`))

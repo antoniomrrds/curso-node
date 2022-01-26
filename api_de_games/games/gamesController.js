a7d77795768b444745a2199b08774c7e8c9dc779
antoniomrrds <arqueiroverd@gmail.com>
2022-01-26 03:37:31 -0300

    Finalizado o campito de jwt e consumo de APi 26-01-2022

--- api_de_games/games/gamesController.js
+++ api_de_games/games/gamesController.js
@@ -7,27 +7,24 @@ const Games = require('./GamesTable');
 const validation = require('../validation/validation');
 const { empty } = validation();
 
-
 //read 
-router.get('/games', (req, res) => {
-    Games.findAll().then(data => {
-        res.statusCode = 200;
-        res.json(data)
-    }).catch(err => {
-        res.sendStatus(404);
-    })
-
+router.get('/games', async (req, res) => {
+    try {
+        const games = await Games.findAll();
+        (games == null || games.length == 0) ? res.sendStatus(404) : res.status(200).json(games);
+    } catch (err) {
+        res.sendStatus(500)
+    }
 });
 
 //create
-router.post('/game', (req, res) => {
+router.post('/game', async (req, res) => {
     const { title, price, year } = req.body;
     let obj = {
         title: title,
         year: year,
         price: price,
     }
-
     const keysObj = Object.keys(obj)
 
     const arrayData = keysObj.every(el => obj[el] != undefined)
@@ -36,112 +33,92 @@ router.post('/game', (req, res) => {
         if (empty(obj.title)) {
             res.sendStatus(400);
         } else {
-            Games.create({
-                title: title,
-                year: year,
-                price: price
-            })
-                .then(
-                    console.log('Game Criado !!!'),
-                    res.sendStatus(201)
-                )
-                .catch(err => {
-                    console.log('Ops houve um erro !!!' + err)
-                    res.sendStatus(404);
-                })
-
+            try {
+                const gameCreated = await Games.create(obj)
+                res.sendStatus(201);
+            } catch (err) {
+                res.sendStatus(500);
+            }
         }
-
     } else {
         res.sendStatus(400);
     }
-
-
 })
 
 //read id
-router.get("/game/:id", (req, res) => {
+router.get("/game/:id", async (req, res) => {
     let id = req.params.id;
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
-        Games.findOne({
-            where: {
-                id: id
-            }
-        }).then(data => {
-            res.statusCode = 201;
-            res.json(data);
-        }).catch(err => {
-            res.sendStatus(404)
-        })
+        try {
+            const gameId = await Games.findOne({ where: { id: id } });
+            (gameId == null) ? res.sendStatus(404) : res.status(200).json(gameId);
+        } catch (err) {
+            res.sendStatus(500)
+        }
 
     }
 })
 
 
 // update
-router.put('/game/:id', (req, res) => {
+router.put('/game/:id', async (req, res) => {
 
     const id = req.params.id;
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
-        const { title, year, price } = req.body;
-        let obj = {
-            title: title,
-            year: year,
-            price: price,
-        }
-        const keysObj = Object.keys(obj)
+        const gameId = await Games.findOne({ where: { id: id } })
+        if (gameId == null) {
+            res.sendStatus(404);
+        } else {
+            const { title, year, price } = req.body;
+            let obj = {
+                title: title,
+                year: year,
+                price: price,
+            }
+            const keysObj = Object.keys(obj)
 
-        const arrayData = keysObj.every(el => obj[el] != undefined)
+            const arrayData = keysObj.every(el => obj[el] != undefined)
 
-        if (arrayData) {
-            if (empty(obj.title)) {
-                res.sendStatus(400)
-            } else {
-                Games.update(
-                    {
-                        title: title,
-                        year: year,
-                        price: price,
-                    }, {
-                    where: {
-                        id: id
+            if (arrayData) {
+                if (empty(obj.title)) {
+                    res.sendStatus(400)
+                } else {
+                    try {
+                        const gameUpdate = await Games.update(obj, { where: { id: id } })
+                        res.sendStatus(200);
+                    } catch (err) {
+                        res.sendStatus(500);
                     }
                 }
-                ).then(_ => {
-                    res.sendStatus(200);
-                }).catch(err => {
-                    res.sendStatus(404);
-                })
+            } else {
+                res.sendStatus(400)
             }
-
-        } else {
-            res.sendStatus(400)
         }
-
     }
 })
 
 //delete
 
-router.delete('/game/:id', (req, res) => {
+router.delete('/game/:id',async (req, res) => {
     const id = req.params.id
     if (isNaN(id)) {
         res.sendStatus(400)
     } else {
-        Games.destroy({
-            where:{
-                id:id
-            }
-        }).then(_=>{
-            res.sendStatus(200);
-        }).catch(err => {
+        const gameId = await Games.findOne({ where: { id: id } })
+        if (gameId == null) {
             res.sendStatus(404);
-        })
-
+        } else{
+            try{
+                const  gameDelete =await Games.destroy({where: {id: id}})
+                res.sendStatus(200);
+            }catch(err){
+                res.sendStatus(404);
+            }
+        }
     }
 })
 
--- api_de_games/index.js
+++ api_de_games/index.js
@@ -12,9 +12,7 @@ connection
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use('/',gamesController)
-
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Servidor esta rodando na porta : ${PORT} !!!`))
